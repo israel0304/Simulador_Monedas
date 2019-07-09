513e9fd04abf6f2104ecf1f26b13ba108a422ff8
israel0304 <israel0304@gmail.com>
2019-07-09 08:21:11 -0500

    agregando código de simulación de monedas

---
+++ js/main.js
@@ -0,0 +1,66 @@
+m=function(m){
+  console.log(m);
+};
+d=function(d){
+  document.write(d);
+};
+
+class Cara{
+  lado(min,max){
+    return parseInt(Math.random()*(max-min)+min);
+  }
+}
+
+class LanzarM{
+  lanzar(nl=1){
+    this.recoger=[0,0];
+      for(this.i=0;this.i<nl;this.i++){
+        this.lados = new Cara().lado(2,0);
+        this.evaluar=(this.lados===0)?this.recoger[0]++:this.recoger[1]++;
+       }
+       return this.recoger;
+    }
+}
+
+class Simulador{
+  constructor(nm=1,nl=1){
+    this.monedas=[];
+    this.nm=nm;
+    this.nl=nl;
+  }
+  
+  simular(){
+    for(this.i=0;this.i<this.nm;this.i++){
+      this.sim = new LanzarM();
+      this.sim.lanzar(this.nl);
+      this.a=this.sim.recoger[0];
+      this.s=this.sim.recoger[1];
+      this.monedas.push({moneda:[this.a,this.s]});
+    }
+    return this.monedas;
+  }
+}
+
+ mml= new Simulador(1,10);
+ monedas = mml.simular();
+ m(monedas);
+ valores = monedas.map(function (monedas){
+   return monedas.moneda;
+ });
+ 
+ valores.forEach(function(valor,indice){
+   d(valor+"<br>");
+ });
+ 
+ 
+
+/*
+   valores = monedas.map(function(monedas){
+      return monedas.moneda;
+    });
+    
+    valores.forEach(function(valor,indice){
+      d(valor+"<br>");
+    });
+  
+  */
